a521ed4e57a2bcbea64295062625773ee385d00c
Andy Meneely <andy@se.rit.edu>
2021-03-06 01:11:31 -0500

    port the editorial checkers

--- spec/cve_spec.rb
+++ spec/cve_spec.rb
@@ -12,91 +12,22 @@ describe 'CVE yml file' do
 
       let(:vuln) { YAML.load(File.open(file)) }
 
-      it('has all the proper keys key')   { expect(vuln).to include('CVE') }
-      it('has fixes key') { expect(vuln).to include('fixes') }
-      it('has vccs key')  { expect(vuln).to include('vccs') }
+      it('has all the proper keys')  do
+        expect(vuln).to include('fixes')
+        expect(vuln).to include('CVE')
+        expect(vuln).to include('vccs')
+      end
 
-      it 'has valid git hashes and commit/note structure in fixes, vccs, and interesting_commits' do
+      it 'has valid 40-char git hashes and commit/note structure in fixes, vccs, and interesting_commits' do
         vuln['fixes'].each do |fix|
-          expect(fix['commit']).to(match(/[0-9a-f]{40,}/).or(be_nil))
+          expect(fix['commit'].to_s).to(match(/[0-9a-f]{40,}/).or(be_empty))
         end
         vuln['vccs'].each do |fix|
-          expect(fix['commit']).to(match(/[0-9a-f]{40,}/).or(be_nil))
+          expect(fix['commit'].to_s).to(match(/[0-9a-f]{40,}/).or(be_empty))
         end
         vuln['interesting_commits']['commits'].each do |fix|
-          expect(fix['commit']).to(match(/[0-9a-f]{40,}/).or(be_nil))
+          expect(fix['commit'].to_s).to(match(/[0-9a-f]{40,}/).or(be_empty))
         end
-      end
-
-      context 'when curated, it must' do
-
-        it 'have the CWE filled out' do
-          if(vuln['curated'])
-            expect(vuln['CWE'].to_s).not_to be_empty
-          end
-        end
-
-        it 'have a description and mistakes made written' do
-          if(vuln['curated'])
-            expect(vuln['description'].to_s).not_to be_empty
-            expect(vuln['mistakes']['answer'].to_s).not_to be_empty
-          end
-        end
-
-        it 'have answers for unit_tested questions' do
-          if(vuln['curated'])
-            expect(vuln['unit_tested']['code']).to be(true).or(be(false))
-            expect(vuln['unit_tested']['code_answer'].to_s).not_to be_empty
-            expect(vuln['unit_tested']['fix']).to be(true).or(be(false))
-            expect(vuln['unit_tested']['fix_answer'].to_s).not_to be_empty
-          end
-        end
-
-        it 'have answers discovered and autodiscoverable' do
-          if(vuln['curated'])
-            expect(vuln['discovered']['answer'].to_s).not_to be_empty
-            expect(vuln['discovered']['automated']).to be(true).or(be(false))
-            expect(vuln['discovered']['contest']).to be(true).or(be(false))
-            expect(vuln['discovered']['developer']).to be(true).or(be(false))
-            expect(vuln['autodiscoverable']['answer_note'].to_s).not_to be_empty
-            expect(vuln['autodiscoverable']['answer']).to be(true).or(be(false))
-          end
-        end
-
-        it 'have answers for specification' do
-          if(vuln['curated'])
-            expect(vuln['specification']['answer_note'].to_s).not_to be_empty
-            expect(vuln['specification']['answer']).to be(true).or(be(false))
-          end
-        end
-
-        it 'have properly formatted subsystem names' do
-          if(vuln['curated'])
-            expect(vuln['subsystem']['answer'].to_s).not_to be_empty
-
-            subsystem_str = Array[vuln['subsystem']['name']].join
-            expect(subsystem_str).to match(/^[a-zA-Z\s0-9\_\-\@]+$/)
-          end
-        end
-
-        it 'have answers for i18n' do
-          if(vuln['curated'])
-            expect(vuln['i18n']['note'].to_s).not_to be_empty
-            expect(vuln['i18n']['answer']).to be(true).or(be(false))
-          end
-        end
-
-        it 'have answers for ipc' do
-          if(vuln['curated'])
-            expect(vuln['ipc']['note'].to_s).not_to be_empty
-            expect(vuln['ipc']['answer']).to be(true).or(be(false))
-          end
-        end
-
-      end
-
-      it 'has an empty nickname or under 30 chars' do
-        expect(vuln['nickname'].to_s.length).to be <= 30
       end
 
       it 'has lessons properly formatted' do
@@ -135,8 +66,73 @@ describe 'CVE yml file' do
                                                                or(be_nil)
       end
 
+      it 'has an empty nickname or under 30 chars' do
+        expect(vuln['nickname'].to_s.length).to be <= 30
+      end
+
+      context 'when curated at level 1, it must' do
+        it 'have the CWE filled out' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['CWE'].to_s).not_to be_empty
+          end
+        end
+
+        it 'have a description and mistakes made written' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['description'].to_s).not_to be_empty
+            expect(vuln['mistakes']['answer'].to_s).not_to be_empty
+          end
+        end
+
+        it 'have answers for unit_tested questions' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['unit_tested']['code']).to be(true).or(be(false))
+            expect(vuln['unit_tested']['code_answer'].to_s).not_to be_empty
+            expect(vuln['unit_tested']['fix']).to be(true).or(be(false))
+            expect(vuln['unit_tested']['fix_answer'].to_s).not_to be_empty
+          end
+        end
+
+        it 'have answers discovered and autodiscoverable' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['discovered']['answer'].to_s).not_to be_empty
+            expect(vuln['discovered']['automated']).to be(true).or(be(false))
+            expect(vuln['discovered']['contest']).to be(true).or(be(false))
+            expect(vuln['discovered']['developer']).to be(true).or(be(false))
+            expect(vuln['autodiscoverable']['answer_note'].to_s).not_to be_empty
+            expect(vuln['autodiscoverable']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have answers for specification' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['specification']['answer_note'].to_s).not_to be_empty
+            expect(vuln['specification']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have properly formatted subsystem names' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['subsystem']['answer'].to_s).not_to be_empty
+            subsystem_str = Array[vuln['subsystem']['name']].join
+            expect(subsystem_str).to match(/^[a-zA-Z\s0-9\_\-\@]+$/)
+          end
+        end
+
+        it 'have answers for i18n' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['i18n']['note'].to_s).not_to be_empty
+            expect(vuln['i18n']['answer']).to be(true).or(be(false))
+          end
+        end
+
+        it 'have answers for ipc' do
+          if at_curation_level?(vuln, 1)
+            expect(vuln['ipc']['note'].to_s).not_to be_empty
+            expect(vuln['ipc']['answer']).to be(true).or(be(false))
+          end
+        end
+      end
     end
-
   end
-
 end
